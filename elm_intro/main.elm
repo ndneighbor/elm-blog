fef3766ac2f4788f6869584075afcb6b3b2b23d6
Angelo Saraceno <asara019@fiu.edu>
2017-05-04 19:31:10 -0400

    okay

--- elm_intro/main.elm
+++ elm_intro/main.elm
@@ -1,7 +1,35 @@
 module Greeting exposing (..)
 
-import Html exposing (text, Html)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
+
+-- Main
 
 main : Html a
 main =
-    text "Hello World"
+    view model
+
+--Model
+
+model : String
+model = 
+    "World"
+
+--View
+
+view : String -> Html a
+view person = 
+    div []
+     [ h2 [] [ text ("Todo List For: " ++ person) ]
+     , ul []
+       [ li [] [ text "Sample Todo Item" ]
+       ]
+     , input 
+       [type_ "text"
+       , placeholder "New Todo Item"
+       ] []
+     , button [] [ text "Add Item" ]
+     ]
+
+
